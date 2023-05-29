85956a82d8ba8da9eab5a2884cfa365c0975d668
Cdcod <thaiphgcs210953@fpt.edu.vn>
2023-05-29 15:54:57 +0700

    update

--- Lab_5/app_mid.js
+++ Lab_5/app_mid.js
@@ -1,8 +1,12 @@
 const express = require ( 'express' );
+const date = require('date-and-time')
+
 const app = express();
+const now = new Date();
 
 app.use((req, res, next) => {
-	console.log( 'Time:' , Date.now());
+	const value = date.format(now,'YYYY/MM/DD HH:mm:ss');
+	console.log("current date and time : " + value)
 	next();
 });
 
